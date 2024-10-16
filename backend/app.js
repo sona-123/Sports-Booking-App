59d90c07d157dd77d836a0be58a5f1d78d5d7c57
Sonali Gupta <sonalibyahut0125@gmail.com>
2024-10-16 12:46:33 +0530

    Added server

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+require('dotenv').config();
+
+const bookingRoutes = require('./routes/bookingRoutes');
+const centerRoutes = require('./routes/centerRoutes');
+const sportRoutes = require('./routes/sportRoutes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+// Routes
+app.use('/api/bookings', bookingRoutes);
+app.use('/api/centers', centerRoutes);
+app.use('/api/sports', sportRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+module.exports = app;
